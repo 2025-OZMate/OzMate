5b954366a16225b6291915d6b5fc61af36537bdc
juyeon Bae <w2316@e-mirim.hs.kr>
2025-01-13 09:37:33 +0900

    fest : test main page 구현

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,8 +1,27 @@
 import React from "react";
+import BackButton from "../components/Details/BackButton"
+import Card from "../components/PracitceTest/Card"
+import SituationCard from "../components/PracitceTest/SituationCard";
+const titles = [
+    { subject: "English practice", title: "Conversation practice", description: "You can learn expressions", navigateTo: "../../src/pages/Practice/SituationSelect" },
+    { subject: "Quiz", title: "Australian Culture Quiz", description: "You can take fun quizzes about Australia!" },
+]
 
 const Test = () => {
     return (
-        <h1>This is test page</h1>
+        <div className="all-container">
+            <BackButton />
+            {titles.map((item, index) => (
+                <Card
+                    key={index}
+                    subject={item.subject}
+                    title={item.title}
+                    description={item.description}
+                    navigateTo={item.navigateTo}
+                />
+            ))}
+
+        </div>
     )
 }
 export default Test;
