66c7cd95226009dcffb4bab9faa3b4a3836f554a
juyeon Bae <w2316@e-mirim.hs.kr>
2025-01-13 09:43:26 +0900

    comment : import link 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Test from "../src/pages/Test"
-import Home from "../src/pages/Home"
-import Mypage from "../src/pages/Mypage"
-import BottomNav from "../src/components/Details/BottomNav"
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Login from "../src/pages/Login/Login";
+import Test from "../src/pages/Test";
+import Home from "../src/pages/Home";
+import SituationSelect from "./pages/Practice/SituationSelect";
+import Mypage from "../src/pages/Mypage";
+import BottomNav from "../src/components/Details/BottomNav";
+import Quiz from "./pages/Practice/Quiz";
+import reset from "../src/styles/Style.module.css";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/Test" element={<Test />}></Route>
-        <Route path="/Home" element={<Home />}></Route>
-        <Route path="/Mypage" element={<Mypage />}></Route>
-      </Routes>
-      <BottomNav></BottomNav>
-
+      <Main />
     </Router>
   );
 };
 
+const Main = () => {
+  const location = useLocation();
+  const hideBottomNavPaths = ["/Login", "/Quiz", "/SituationSelect"];
+
+  return (
+    <>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Test" element={<Test />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Mypage" element={<Mypage />} />
+        <Route path="/SituationSelect" element={<SituationSelect />} />
+        <Route path="/Quiz" element={<Quiz />} />
+      </Routes>
+      {!hideBottomNavPaths.includes(location.pathname) && <BottomNav />}
+    </>
+  );
+};
+
 export default App;
-
-
