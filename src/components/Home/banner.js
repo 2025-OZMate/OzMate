3734c3bb4a22c7f3b4ecaf8cec5905dcb8e4e84c
juyeon Bae <w2316@e-mirim.hs.kr>
2025-01-16 00:54:35 +0900

    style : add className

--- src/components/Home/banner.js
+++ src/components/Home/banner.js
@@ -4,6 +4,7 @@ export default function Banner() {
     return (
         <div className={styles.bannerContainer}>
             <img src="/images/train.png" style={{ height: "375px" }} alt="Train" />
+            <div className={styles["logo-white"]}> <img src="/images/logo-white.png"  ></img></div>
             <div className={styles.textContainer}>
                 <div>Types of Transportation</div>
                 <div>Cards in Australia</div>
