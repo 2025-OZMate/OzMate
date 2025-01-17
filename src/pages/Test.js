3fbb36b32bde2862509508505f8055e53c3481f6
juyeon Bae <w2316@e-mirim.hs.kr>
2025-01-17 23:33:57 +0900

    fix : navigate 삭제

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,9 +1,7 @@
 import React from "react";
-import BackButton from "../components/Details/BackButton"
 import Card from "../components/PracitceTest/Card"
-import SituationCard from "../components/PracitceTest/Speaking/SituationCard";
 const titles = [
-    { subject: "English practice", title: "Conversation practice", description: "You can learn expressions", navigateTo: "../../src/pages/Practice/SituationSelect" },
+    { subject: "English practice", title: "Conversation practice", description: "You can learn expressions" },
     { subject: "Quiz", title: "Australian Culture Quiz", description: "You can take fun quizzes about Australia!" },
 ]
 
@@ -23,7 +21,6 @@ const Test = () => {
                     subject={item.subject}
                     title={item.title}
                     description={item.description}
-                    navigateTo={item.navigateTo}
                 />
             ))}
 
