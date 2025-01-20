c83c768894afebb6201ec33ee57ddead9120dcc0
juyeon Bae <w2316@e-mirim.hs.kr>
2025-01-20 14:15:53 +0900

    feat : progress bar 컴포넌트 구현

---
+++ src/components/PracitceTest/Trans/ProgressBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProgressContainer = styled.div`
+  width: 100%;
+  max-width: 287px;
+  background-color: #FFF59D;
+  border-radius: 10px;
+  padding : 3px 0 3px 3px;
+  margin: 20px 0;
+`;
+
+
+const Progress = styled.div`
+  width: ${({ progress }) => progress}%;
+  height: 10px;
+  background-color: #FFB600;
+  border-radius: 10px;
+  transition: width 0.3s ease-in-out;
+`;
+
+export default function ProgressBar({ currentQuestion, questionsLength }) {
+  const progress = ((currentQuestion + 1) / questionsLength) * 100;
+
+  return (
+    <ProgressContainer>
+      <Progress progress={progress} />
+    </ProgressContainer>
+  );
+}
