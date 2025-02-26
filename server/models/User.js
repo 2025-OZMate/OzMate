760c1d935cb603274e158c1fe7e79ad6fb97b03b
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-27 02:20:58 +0900

    feat : 북마크 추가, 삭제, 조회 API

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,20 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     userid: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+
+    //북마크 배열
+    bookmarks: [
+        {
+            title: String,
+            thumbnail: String,
+            description: String,
+            category: String,
+        }
+    ]
+
+
 });
+
+
 
 module.exports = mongoose.model("User", UserSchema);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -54,5 +54,59 @@ router.post("/login", async (req, res) => {
     }
 });
 
+// 북마크 추가/삭제 API
+router.post("/toggleBookmark", async (req, res) => {
+    const { userid, bookmark } = req.body;
+
+    try {
+        const user = await User.findOne({ userid: userid });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // 북마크 배열이 없는 경우 초기화
+        if (!user.bookmarks) {
+            user.bookmarks = [];
+        }
+
+        // 북마크가 이미 존재하는지 확인
+        const existingBookmarkIndex = user.bookmarks.findIndex(
+            (b) => b.title === bookmark.title
+        );
+
+        if (existingBookmarkIndex === -1) {
+            user.bookmarks.push(bookmark); // 없으면 추가
+        } else {
+            user.bookmarks.splice(existingBookmarkIndex, 1); // 있으면 삭제
+        }
+
+        // 데이터가 저장된 후에 응답을 보냄
+        await user.save(); // 저장이 끝난 후 응답 보내기
+        console.log('저장된 user', user)
+
+        res.status(200).json(user.bookmarks);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+});
+
+// 특정 사용자의 북마크 조회 API
+router.get('/getUserBookmarks/:userId', async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        // 사용자 및 북마크 조회
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user.bookmarks); // 사용자의 북마크 목록 반환
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: '내부 서버 오류' });
+    }
+});
 
 module.exports = router;
