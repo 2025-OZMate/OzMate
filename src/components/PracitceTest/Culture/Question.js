e0819b5ffeb85d51a17de31962aadc1ae87792c6
juyeon Bae <w2316@e-mirim.hs.kr>
2025-02-27 15:17:37 +0900

    design : test 배경 로고 및 font, border 색 변경

--- src/components/PracitceTest/Culture/Question.js
+++ src/components/PracitceTest/Culture/Question.js
@@ -3,17 +3,22 @@ import styles from "../../../styles/Practice/Quiz.module.css"
 const Question = ({ questionIndex, question, choices, onChoiceClick }) => {
     return (
         <div className={styles["all-container"]}>
-            <div className={styles["question-container"]}>
-                <h3>Quiz. 0{questionIndex + 1}</h3>
-                <span className={styles.question}>{question}</span>
+            <img className={styles.quizBannerImg} src="/images/quizBanner.png"></img>
+            <img className={styles.bannerLogoImg} src="/images/bannerLogo.png"></img>
+
+            <div className={styles.contentContainer}>
+                <div className={styles["question-container"]}>
+                    <h3>Quiz. 0{questionIndex + 1}</h3>
+                    <span className={styles.question}>{question}</span>
+                </div>
+                <ul className={styles["choice-container"]}>
+                    {choices.map((option, index) => (
+                        <li key={index} onClick={() => onChoiceClick(option)} className={styles.choice}>
+                            {option}
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <ul className={styles["choice-container"]}>
-                {choices.map((option, index) => (
-                    <li key={index} onClick={() => onChoiceClick(option)} className={styles.choice}>
-                        {option}
-                    </li>
-                ))}
-            </ul>
         </div>
     );
 };
