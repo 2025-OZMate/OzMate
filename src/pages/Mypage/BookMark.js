03cf5cdcc1fe7940c7519700f63549defb1152c2
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-01 05:04:08 +0900

    feat : 북마크 정보 가져오는 api 연결

--- src/pages/Mypage/BookMark.js
+++ src/pages/Mypage/BookMark.js
@@ -1,15 +1,51 @@
-import React, { useEffect, useState } from "react";
-import BackButton from "../../components/Details/BackButton";
-import TitleHeader from "../../components/Mypage/titleHeader";
-import InformationCard from "../../components/Home/InformationCard";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function BookMark() {
+import InformationCard from "../../components/Home/InformationCard"
+import BackButton from "../../components/Details/BackButton"
+import TitleHeader from "../../components/Mypage/titleHeader"
 
+export default function Bookmarks() {
+    const [bookmarks, setBookmarks] = useState([]);
+    const userId = localStorage.getItem("userObjectId");
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        // 서버에서 북마크 정보 가져오기
+        if (userId) {
+            axios.get(`http://localhost:5000/auth/getUserBookmarks/${userId}`)
+                .then(response => {
+                    console.log("가져온 data: ", response.data)
+                    setBookmarks(response.data);
+                })
+                .catch(error => console.error("북마크 불러오기 실패:", error));
+        }
+    }, [userId]);
+
+    const handleCardClick = (id, e) => {
+        if (!e.target.closest('button')) {
+            navigate(`/detail/${id}`)
+        }
+    }
     return (
-        <div>
+        <div  >
             <BackButton />
-            <TitleHeader title="Bookmark List" />
-
+            <TitleHeader title={"BookMark"} />
+            <div style={{ padding: "0 20px 30px", display: "flex", flexDirection: "column", gap: "12px" }}>
+                {bookmarks.map((bookmark, index) => (
+                    <InformationCard
+                        key={bookmark.id}
+                        id={bookmark.id}
+                        src={bookmark.thumbnail}
+                        title={bookmark.title}
+                        description={bookmark.description}
+                        type={bookmark.category}
+                        onClick={(e) => handleCardClick(bookmark.id, e)}
+                    />
+                ))}
+            </div>
         </div>
-    )
+    );
 }
